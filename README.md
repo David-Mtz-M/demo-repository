9d56943a23b9139bac2b3de6632b68e97c3f4abb
David-Mtz-M <91239383+David-Mtz-M@users.noreply.github.com>
2021-11-23 22:28:20 -0600

    Cambio de prueba
     se añadió un archivo .cpp y se modificó el readme.

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Una demo para practicar
 
 
 primer cambio
+
+## agregando cosas desde vs code
+
+jajjaaja si jalo
+esta madre
+xd
---
+++ archivo2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "hola hijo mio";
+
+
+    return 0;
+}
